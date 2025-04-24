41017151e896ca2db3b28b08fbad4ea67b480afd
nargisDia <126104947+nargisDia@users.noreply.github.com>
2025-04-24 17:56:02 +0600

    Add files via upload

---
+++ API_Integration/scripts.js
@@ -0,0 +1,26 @@
+const imgContainer = document.querySelector
+('#image-container');
+const urlAPI = 'https://picsum.photos/v2/list';
+
+
+const getPicture = () =>{
+    fetch(urlAPI).then(data => data.json()).then(items =>{
+        console.log(items);
+        items.forEach(function(photoObj){
+            const newContainer = document.createElement('div');
+            newContainer.classList.add('image-container-item');
+
+
+            const newImg =document.createElement('img');
+            newImg.src = photoObj.download_url;
+            newContainer.appendChild(newImg);
+
+            const newAuthor = document.createElement('p');
+            newAuthor.textContent = photoObj.author;
+
+           newContainer.appendChild(newAuthor);
+           imgContainer.appendChild(newContainer);
+        });
+    });
+};
+getPicture();
